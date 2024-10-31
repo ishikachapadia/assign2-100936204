659c8164ac2a64959303c6776c81281d598c9f71
Ishika Chapadia <ishikachapadia@gmail.com>
2024-10-31 11:07:03 -0400

    added site icon and custom theme

--- app/public/wp-content/themes/twentytwentyfour-child/functions.php
+++ app/public/wp-content/themes/twentytwentyfour-child/functions.php
@@ -1,5 +1,5 @@
 <?php
-// Enqueue parent theme styles
+
 function my_theme_enqueue_styles() {
     $parent_style = 'twentytwentyfour-style'; // Parent theme's style handle.
 
@@ -7,3 +7,11 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+// Add support for menus
+function my_theme_setup() {
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'twentytwentyfour-child'),
+    ));
+}
+add_action('after_setup_theme', 'my_theme_setup');
